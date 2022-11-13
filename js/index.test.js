eccb4a0a25059ba3a631fd05860d1cb4bc881660
Quico <federicodesantiago@gmail.com>
2022-11-13 12:23:04 +0100

    hotfix/test

--- js/index.test.js
+++ js/index.test.js
@@ -2,14 +2,14 @@ import { arrayLength } from './index.js';
 
 //Testing array.length
 
-const dataLength = [
+const dataLength2 = [
     [1, 2, 3, 4, 5],
     ['a', 'hola', 89],
     [undefined, undefined, null, null],
     [{}, 2, null, 4],
 ];
 
-dataLength.forEach((data) => {
+dataLength2.forEach((data) => {
     describe(`When argument are ${data}`, () => {
         test(`Then the result should be ${data.length}`, () => {
             const r = arrayLength(data);
